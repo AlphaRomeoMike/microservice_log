20e2677af0d8ac33c6e262ee7b8da48ed2cc853a
AlphaRomeoMike <74718326+AlphaRomeoMike@users.noreply.github.com>
2023-09-17 15:30:09 +0500

    - Deleted rotues, now handling logic with amqp

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 import express, { Application } from 'express'
 import { config } from './config/index.config';
 import { middlwares } from './middlewares/index.middleware'; 
-import router from './routes/index.routes';
 
 // Destructuring
 const { env } = config;
@@ -11,7 +10,6 @@ const app: Application = express();
 
 app.use(express.json());
 app.use(log_middlware);
-app.use(router);
 
 app.listen(env.PORT || 3001, () => {
   console.info(`App listening on port ${env.PORT || 3001}`);
--- src/routes/index.routes.ts
+++
@@ -1,7 +0,0 @@
-import express, { Router } from 'express'
-
-const router = express.Router();
-
-router.all('*');
-
-export default router;
